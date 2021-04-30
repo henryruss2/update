1ee75e0579795aeecbf4cecb8c162f427793afef
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2021-04-30 08:27:10 -0400

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,5 +1,5 @@
 import requests
-selfVersion = 1
+selfVersion = 3
 version = int(requests.get('https://henryruss2.github.io/update/version'))
 if (selfVersion < version):
     update = requests.get('https://henryruss2.github.io/update/browser.py')
@@ -9,5 +9,5 @@ if (selfVersion < version):
 while True:
     x = requests.get(input('Enter a URL:\n'))
     x = x.text
-    print('loaded!')
+    print('Site loaded\n')
     print(x)
