50b8747fdc238b074993285e262eeac1de3f3ed0
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2021-04-30 11:42:07 -0400

    Update browser.py

--- browser.py
+++ browser.py
@@ -4,7 +4,7 @@ version = int(str(requests.get('https://henryruss2.github.io/update/version.txt'
 if (selfVersion < version):
     update = requests.get('https://henryruss2.github.io/update/browser.py')
     f = open("browser.py", "w")
-    f.write(update)
+    f.write(update.text)
     f.close()
 while True:
     x = requests.get(input('Enter a URL:\n'))
