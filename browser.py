ee529d12941c7c58ab56b72aa306e6bed26c412e
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2021-04-30 08:26:33 -0400

    Add files via upload

--- browser.py
+++ browser.py
@@ -1,5 +1,13 @@
+import requests
+selfVersion = 1
+version = int(requests.get('https://henryruss2.github.io/update/version'))
+if (selfVersion < version):
+    update = requests.get('https://henryruss2.github.io/update/browser.py')
+    f = open("browser.py", "w")
+    f.write(update)
+    f.close()
 while True:
-    import requests
     x = requests.get(input('Enter a URL:\n'))
     x = x.text
+    print('loaded!')
     print(x)
